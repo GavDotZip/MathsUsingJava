4f49cafbb9fd051733bb08fd305eb4643058358d
Gavin Carberry <98050479+GavDotZip@users.noreply.github.com>
2023-05-06 13:37:45 +0100

    added comments

--- postfixNotation/stackArray.java
+++ postfixNotation/stackArray.java
@@ -1,20 +1,24 @@
 import javax.swing.JOptionPane;
 
 public class stackArray implements postfixStack{
+    //basic data declarations for variables
 	protected Object S[];
 	protected static final int CAPACITY = 1000;
 	protected int capacity;
 	protected int top = -1;
 	
+    //constructor
 	public stackArray() {
 		this(CAPACITY);
 	}
 	
+    //constructor to set capacity for stack
 	public stackArray(int cap) {
 		capacity = (cap > 0) ? cap : CAPACITY;
 		S = new Object[capacity];
 	}
 	
+    //override methods from interface
 	public boolean isEmpty() {
 		return (top < 0);
 	}
